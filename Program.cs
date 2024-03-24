153c504e59f28133baf8cb726253b8c4288a036c
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-24 17:43:15 -0300

    Projeto inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PodCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
