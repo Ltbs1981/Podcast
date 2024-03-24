44afb9ce62b4d528a541395d06b3dfb0212f9da6
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-24 18:25:16 -0300

    Finalizada a  especificação da classe episódio

---
+++ Episodio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PodCast
+{
+    class Episodio
+    {
+        public int Ordem { get; }
+        public string  Titulo { get; }
+        public int Duracao  { get; }
+        public string Resumo  { get; set; }
+
+
+
+    }
+}
